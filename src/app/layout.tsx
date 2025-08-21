7ac64c21f9ef1c10bb9bbac68e85e4a1f81b1ee8
emirhan.bayatli <emirhan.bayatli99@icloud.com>
2025-08-22 00:07:06 +0300

    fix: Remove unused font imports from layout component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,9 @@
 import type { Metadata } from "next";
-//import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { CartContextProvider } from "./CartContextProvider";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import { AuthContextProvider } from "./AuthContextProvider";
-// const geistSans = Geist({
-//   variable: "--font-geist-sans",
-//   subsets: ["latin"],
-// });
-
-// const geistMono = Geist_Mono({
-//   variable: "--font-geist-mono",
-//   subsets: ["latin"],
-// });
 
 export const metadata: Metadata = {
   title: "E-Commerce",
