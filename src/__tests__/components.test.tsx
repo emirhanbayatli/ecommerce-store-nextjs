b8f10a5f156aaa4e3aeb0ba4b74853a33a68a0b2
emirhan.bayatli <emirhan.bayatli99@icloud.com>
2025-08-28 17:16:23 +0300

    refactor: simplify Button component test by removing unnecessary variable

--- src/__tests__/components.test.tsx
+++ src/__tests__/components.test.tsx
@@ -6,9 +6,7 @@ const handleSubmitMock = jest.fn();
 
 describe("components test ", () => {
   test("Button component test ", () => {
-    const { container } = render(
-      <Button label="Click" onClick={handleSubmitMock} />,
-    );
+    render(<Button label="Click" onClick={handleSubmitMock} />);
     expect(screen.getByText("Click")).toBeInTheDocument();
   });
 });
